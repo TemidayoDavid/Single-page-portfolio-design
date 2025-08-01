a19b254b87c4aef6ff2623f3d624d708bec8a70c
TemidayoDavid <isitemidayo@gmail.com>
2025-08-01 23:08:22 +0100

    Eighth commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,17 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   const emailInput = document.getElementById("email");
   const errorMessage = document.getElementById("email-error");
   const cautionError = document.getElementById("symbol-error");
-  const form = document.getElementById('myForm');
+
 
   if (!emailInput.validity.valid) {
     event.preventDefault();
 
     if (emailInput.validity.valueMissing) {
       errorMessage.textContent = "Email is required.";
+      errorMessage.classList.add("active");
+      cautionError.classList.add('active');
     } else if (emailInput.validity.typeMismatch) {
+      event.preventDefault();
       errorMessage.textContent = "Sorry, invalid format here.";
       errorMessage.classList.add("active");
       cautionError.classList.add('active');
@@ -20,6 +23,6 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     errorMessage.classList.remove("active");
     cautionError.classList.remove('active');
     emailInput.classList.remove("invalid");
-    form.reset();
+    
   }
 });
