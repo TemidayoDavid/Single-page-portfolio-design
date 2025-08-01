69ea511cb13c31e99ec8ca8feba09178a68a977c
TemidayoDavid <isitemidayo@gmail.com>
2025-08-01 22:13:32 +0100

    Seventh commit

--- dist/single-page-developer-portfolio.380419b9.js
+++ dist/single-page-developer-portfolio.380419b9.js
@@ -670,20 +670,21 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 document.getElementById("myForm").addEventListener("submit", function(event) {
     const emailInput = document.getElementById("email");
     const errorMessage = document.getElementById("email-error");
-    const cautionErrror = document.getElementById("symbol-error");
+    const cautionError = document.getElementById("symbol-error");
+    const form = document.getElementById('myForm');
     if (!emailInput.validity.valid) {
         event.preventDefault();
         if (emailInput.validity.valueMissing) errorMessage.textContent = "Email is required.";
         else if (emailInput.validity.typeMismatch) {
             errorMessage.textContent = "Sorry, invalid format here.";
             errorMessage.classList.add("active");
-            cautionErrror.classList.add('active');
+            cautionError.classList.add('active');
             emailInput.classList.add("invalid");
         }
     } else {
         errorMessage.style.display = "none";
         errorMessage.classList.remove("active");
-        cautionErrror.classList.remove('active');
+        cautionError.classList.remove('active');
         emailInput.classList.remove("invalid");
         form.reset();
     }
